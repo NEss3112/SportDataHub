7f2a173d74804d107462cf57b501c7ec49cbde57
NEss3112 <gamerbro230@gmail.com>
2025-04-02 21:29:08 +0300

    functions and classes for fetching data through fastf1 lib

--- sportdatahub/data_fetcher.py
+++ sportdatahub/data_fetcher.py
@@ -3,12 +3,12 @@ import fastf1
 fastf1.Cache.enable_cache("./cache")
 
 def get_event_schedule(year: int):
-    """Gets event schedule for choosen year."""
+    """Gets event schedule for chosen year."""
     return fastf1.get_event_schedule(year, backend="fastf1")
 
 
 def get_session(year, event, session):
-    """Gets event informatiom from fastf1 for choosen year, event and session."""
+    """Gets event information from fastf1 for chosen year, event and session."""
     session = fastf1.get_session(year, event, session, backend="fastf1")
     session.load()
     return session
