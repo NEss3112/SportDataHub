ffd63a098ebd212303223153311b71f29f79073f
NEss3112 <gamerbro230@gmail.com>
2025-04-09 22:09:40 +0300

    added get_closest_race function

--- sportdatahub-backend/src/main.py
+++ sportdatahub-backend/src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from schemas import RaceSchedule
+from service import get_race_schedule
 import fastf1
 import pandas as pd
 from datetime import datetime
@@ -17,26 +18,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-async def get_race_schedule():
-    current_year = datetime.utcnow().year
-
-    loop = asyncio.get_running_loop()
-    schedule = await loop.run_in_executor(None, fastf1.get_event_schedule, current_year)
-
-    races = []
-    for _, row in schedule.iterrows():
-        race = RaceSchedule(
-                round=row["RoundNumber"],
-                name=row["EventName"],
-                location=row["Location"],
-                country=row["Country"],
-                date=row["EventDate"].to_pydatetime().replace(tzinfo=pytz.UTC),
-                time=row["Session5Date"].strftime("%H:%M") if not pd.isna(row["Session5Date"]) else "TBD"
-                )
-        races.append(race)
-
-    return races
-
 
 @app.get("/api/schedule", response_model=list[RaceSchedule])
 async def get_schedule():
@@ -46,7 +27,7 @@ async def get_schedule():
 @app.get("/api/schedule/next", response_model=RaceSchedule)
 async def get_next_race():
     races = await get_race_schedule()
-    now = datetime.utcnow().replace(tzinfo=pytz.UTC)
+    now = datetime.now(pytz.UTC)
 
     future_races = [race for race in races if race.date > now]
 
@@ -59,3 +40,7 @@ async def get_next_race():
 @app.get("/")
 async def home():
     return {"message":"Welcome to SportDataHub v.0.1"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app)
